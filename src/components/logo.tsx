89778b2c2ad61e000b80c3e7480e379b3466e38f
ashraj05 <ashwani.raj@students.iiit.ac.in>
2025-09-10 06:07:41 +0530

    updated Logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo({ className }: { className?: string }) {
   return (
     <img
-      src="/logo.png"
+      src="/Logo.svg"
       alt="InnerG Logo"
       className={className}
       aria-label="InnerG Logo"
